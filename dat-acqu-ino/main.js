3d17f195a0972e7960ed70ebf811a1cb40d72a52
Nicole-Miranda <nicole.martins@sptech.school>
2026-05-18 16:09:26 -0300

    Retirando o comando de insert no cód arduino

--- dat-acqu-ino/main.js
+++ dat-acqu-ino/main.js
@@ -63,7 +63,6 @@ const serial = async (
             if (sensorBloqueio == 1 && sensorBloqueio != valorAnterior) {
                 await poolBancoDados.execute(
                     'INSERT INTO historico_sensor (dt_registro, status_sensor, fk_sensor) VALUES (CURRENT_TIMESTAMP,?,1)',
-                    `INSERT INTO doca (numero_doca, status_doca, fk_empresa) VALUES (${numero_doca},${status_doca},${fk_empresa})`,
                     [sensorBloqueio]
                 );
                 valorAnterior = sensorBloqueio;
@@ -72,7 +71,6 @@ const serial = async (
             } else if (sensorBloqueio == 0 && sensorBloqueio != valorAnterior) {
                 await poolBancoDados.execute(
                     'INSERT INTO historico_sensor (dt_registro, status_sensor, fk_sensor) VALUES (CURRENT_TIMESTAMP,?,1)',
-                     `INSERT INTO doca (numero_doca, status_doca, fk_empresa) VALUES (${numero_doca},${status_doca},${fk_empresa})`,
                     [sensorBloqueio]
                 );
 
